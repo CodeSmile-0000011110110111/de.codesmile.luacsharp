d6c8dcb19963dda8fa68308c53b8e479334ef374
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-06-13 11:53:49 +0200

    add Lua extension methods

---
+++ Runtime/Lua.Unity/Extensions/LuaStateExt.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (C) 2021-2025 Steffen Itterheim
+// Refer to included LICENSE file for terms and conditions.
+
+using Lua;
+using Lua.Runtime;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+namespace CodeSmile.Luny
+{
+	public static class LuaStateExt
+	{
+		public static ValueTask<LuaValue[]> DoStringAsync(this LuaState state, String source, String? chunkName,
+			LuaTable arguments,
+			CancellationToken cancellationToken = default)
+		{
+			var access = state.RootAccess;
+			access.ThrowIfInvalid();
+			var closure = access.State.Load(source, chunkName ?? source);
+			access.Thread.Stack.Push(arguments);
+			return access.DoClosureAsync(closure, 1, cancellationToken);
+		}
+
+		public static async ValueTask<LuaValue[]> DoClosureAsync(this LuaThreadAccess access, LuaClosure closure,
+			Int32 argumentCount, CancellationToken cancellationToken = default)
+		{
+			access.ThrowIfInvalid();
+			var count = await access.RunAsync(closure, argumentCount, cancellationToken);
+			using var results = access.ReadReturnValues(count);
+			return results.AsSpan().ToArray();
+		}
+	}
+}
---
+++ Runtime/Lua.Unity/Extensions/LuaThreadAccessExt.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (C) 2021-2025 Steffen Itterheim
+// Refer to included LICENSE file for terms and conditions.
+
+using Lua;
+using Lua.Runtime;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CodeSmile.Luny
+{
+	public static class LuaThreadAccessExt
+	{
+
+		public static ValueTask<LuaValue[]> DoStringAsync(this LuaThreadAccess access, string source, string? chunkName, LuaTable environment, CancellationToken cancellationToken = default)
+		{
+			access.ThrowIfInvalid();
+			var closure = access.State.Load(source, chunkName ?? source, environment);
+			return access.DoClosureAsync(closure, cancellationToken);
+		}
+	}
+}
