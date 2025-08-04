b901b4dec358cdeff6f9007f84a3305ff44d9258
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-08-04 16:28:55 +0200

    update copy dll batch since i changed repository

--- CopyLuaCSharpDLLs.bat
+++ CopyLuaCSharpDLLs.bat
@@ -1,4 +1,4 @@
 @echo off
-xcopy "..\Lua-CSharp-Official\src\Lua\bin\Debug\netstandard2.1\*.*" "Runtime\LuaCSharp\lib\netstandard2.1\Debug" /Y /V
-xcopy "..\Lua-CSharp-Official\src\Lua\bin\Release\netstandard2.1\*.*" "Runtime\LuaCSharp\lib\netstandard2.1\Release" /Y /V
+xcopy "..\Lua-CSharp-CodeSmile\src\Lua\bin\Debug\netstandard2.1\*.*" "Runtime\LuaCSharp\lib\netstandard2.1\Debug" /Y /V
+xcopy "..\Lua-CSharp-CodeSmile\src\Lua\bin\Release\netstandard2.1\*.*" "Runtime\LuaCSharp\lib\netstandard2.1\Release" /Y /V
 REM pause
